797f48bfa3abb3472fafa5eae9290bc6c8b173f1
sukizqy0402 <qz2246@columbia.edu>
2016-02-10 23:16:30 -0500

    street2coordinates

---
+++ doc/street2coordinates.R
@@ -0,0 +1,10 @@
+# solving problems with street2coordinates
+library(bitops)
+library(RCurl)
+library(plyr)
+library(rjson)
+library(RDSTK)
+data_test<-c("3260 Henry Hudson Parkway, Bronx, NY 10463")
+geocode<- do.call(rbind, lapply(data, street2coordinates))
+geocode
+
