0bbeda666560aeeed2fb0e593799aeb6617f2085
hy2450 <hy2450@columbia.edu>
2016-02-19 20:46:19 -0500

    Shiny test (add button)
    Shiny test (add button)

--- output/leaflet.R
+++ output/leaflet.R
@@ -2,28 +2,32 @@ library(maps)
 library(data.table)
 library(leaflet)
 library(htmltools)
-library(sp)
-library(rgeos)
-tdata<-fread("NYCT.csv")
-tmap<-leaflet(tdata) %>%
-  #sets the center of the map view and the zoom level;
-  setView(lng=-73.96884112664793,lat =40.78983730268673, zoom=11) %>% 
-  addTiles() %>%
-  addMarkers(popup = ~htmlEscape(Location))#show location when click
-tmap
 
+tdata<-fread("NYCT.csv")
 wifidata<-fread("nycwifi.csv")
-wifimap<-leaflet(wifidata) %>%
-  #sets the center of the map view and the zoom level;
-  setView(lng=-73.96884112664793,lat =40.78983730268673, zoom=11) %>% 
-  addTiles() %>% 
-  addMarkers(lng = ~ Long_, lat = ~ Lat,popup = ~htmlEscape(Location), clusterOptions = markerClusterOptions())
-wifimap
+
+
+
+name <-tdata$Name
+location <- tdata$Location
+
+#separate the location name(bold) and the address(italic) into two lines
+name <- paste("<b>",name,"</b>")
+location <- paste("<i>",location,"</i>")
+content <- paste( name, location, sep =  "<br/>")
+
+#set parameters for the icon
+#the iconAnchor is used for setting the position of the tag when you click on the toilet botton.
+ToiletIcon <- makeIcon(
+  iconUrl = "http://icons.iconarchive.com/icons/icons8/ios7/512/Household-Toilet-Pan-icon.png",
+  iconWidth = 20, iconHeight = 25,
+  iconAnchorX = 10, iconAnchorY = 0
+)
 
 map<-leaflet()%>%
   setView(lng=-73.96884112664793,lat =40.78983730268673, zoom=11) %>% 
   addTiles() %>% 
-  addMarkers(data =tdata, group = "Restrooms",popup = ~htmlEscape(Location)) %>%
+  addMarkers(data =tdata, group = "Restrooms",popup = content, icon=ToiletIcon) %>%
   addMarkers(data = wifidata, group = "Wi-Fi",lng = ~ Long_, lat = ~ Lat,
              popup = ~htmlEscape(Location), clusterOptions = markerClusterOptions())%>%
   # Layers control
@@ -31,8 +35,6 @@ map<-leaflet()%>%
     overlayGroups = c("Restrooms", "Wi-Fi"),
   
     options = layersControlOptions(collapsed = FALSE)
-    )%>%
+    )
   
-  
-  addCircleMarkers(lng=-73.96884112664793,lat =40.78983730268673, color="purple",weight = 2,radius = 35)
 map
---
+++ output/shiny test.R
@@ -0,0 +1,86 @@
+library(shinydashboard)
+library(shiny)
+#library(dplyr)
+library(leaflet)
+library(data.table)
+#library(rgeos)
+l#ibrary(rgdal)
+#library(ggmap)
+
+div(class = "my-class", "Div content")
+div(class = "my-class", p("Paragraph text"))
+header<-dashboardHeader(title = "Search for Restroom")
+        
+siderbar<-dashboardSidebar(
+                sidebarMenu(
+                        menuItem("Map", tabName = "map", icon = icon("fa fa-map")),
+                        menuItem("Chart",tabName = "chart", icon = icon("fa fa-bar-chart"), badgeLabel = "new", badgeColor = "green")
+                )
+        )
+body<-dashboardBody(
+                tabItems(
+                        # First tab content
+                        tabItem(tabName = "map", 
+                                fluidRow(
+                                       leafletOutput("map_output", height=450,width = 800),
+                                        p(),
+                                        textInput("address", "My location:"),
+                                        textInput("range","Choose a range:"),
+                                        actionButton("go", "Go!"),
+                                        p("Click the button to update the value displayed in the main panel.")
+                                )
+                        ),
+                        
+                       
+                        #Second tab content
+                        tabItem(tabName = "chart",
+                                # Boxes need to be put in a row (or column)
+                                fluidRow(
+                                        box(
+                                                title="Histgram", status = "primary",solidHeader = TRUE, 
+                                                background="yellow", plotOutput("plot1", height = 250)
+                                           ),
+                                        
+                                        box(
+                                                title = "Controls", status = "warning", solidHeader = TRUE,
+                                                #inputId="slider"
+                                                sliderInput("slider", "Number of observations:", 1, 100, 50),
+                                                textInput("text", "Text input:")
+                                           )
+                                         )
+                                )
+                             )
+                       )        
+                        
+ui<-dashboardPage(header,siderbar,body, title = "Simple Shiny",skin="purple")
+
+server <- function(input, output) {
+        set.seed(122)
+       
+       d_test<-geocode("3260 Henry Hudson Parkway,Bronx")
+       content <- paste(sep ="<br/>","3260 Henry Hudson Parkway","Bronx,NY 10463")
+       
+       my_address<-eventReactive(input$go,{input$address}
+                                 )
+       
+       output$map_output <- renderLeaflet({map})
+       
+       
+       observe({
+           code<-geocode(my_address())
+           leafletProxy("map_output") %>%
+            clearShapes()%>%
+           #run if we get address put in else show my address 
+           setView(code$lon, code$lat,zoom=14)%>%
+           addPopups(code$lon, code$lat,my_address(), 
+                     options = popupOptions(closeButton = FALSE))
+           
+       })
+       
+       
+       
+       
+       
+}
+
+shinyApp(ui, server)
