d541116312e70857a3f7d314f4529273865b3cb2
ZHIBO <zw2327@columbia.edu>
2016-02-20 17:40:29 -0500

    need add output

--- output/dataprocess.py
+++ output/dataprocess.py
@@ -1,7 +1,14 @@
 import csv
 import string 
+import unicodedata
+import geocoder
+import urllib3
+
 
 from geopy.geocoders import Nominatim
+
+#urllib3.disable_warnings()
+#logging.captureWarnings(True)
 geolocator = Nominatim()
 
 with open("../data/NYCT.csv") as csvfile:
@@ -10,17 +17,48 @@ with open("../data/NYCT.csv") as csvfile:
 	for row in spamreader:
 		#print type(row[2])
 		#addr= row[1][1:len(row[1])-1]+","+row[5][1:len(row[5])-1] 
-		addr=row[1]+","+row[5]	
+		#addr=row[1]+","+row[5]	
 		#loc=geolocator.geocode(addr)
 		#if loc is None:
 		#	print "fail"
 		#else:
 			#print(loc.latitude,loc.longitude,row[6],type(row[6]))
-		point=row[6][0:len(row[6])-1]+","+row[7][0:len(row[6])-1]
-
+		point=row[6][0:len(row[6])-1]+","+row[7][0:len(row[7])-1]
+		#print point
 		loc1=geolocator.reverse(point)
+		addr_str=unicodedata.normalize('NFKD',loc1.address).encode('ascii','ignore')
+		if "New York" not in addr_str:			
+			addr= row[1][0:len(row[1])]+","+row[5][0:len(row[5])] 				
+			#print addr
+			loc=geolocator.geocode(addr)
+			if loc is not None:
+				print ("SUCCED")
+				#print loc.address +"," +"SUCCESSED"
+			else:
+				if "(" in addr:
+					new_addr=row[1][0:row[1].index("(")-1]+","+row[5][0:len(row[5])]
+					loc2=geolocator.geocode(new_addr)
+					if loc2 is not None:
+						print(loc2.latitude,loc2.longitude)
+					else:
+						newer_addr=geocoder.google(new_addr)
+						if newer_addr is not None:
+							print (newer_addr.latlng)
+						else:
+							print (new_addr+"----"+addr+"---------"+ row[2]+","+"NEW ADDRESS FAILED")
+				else:		
+					newer1_addr=geocoder.google(addr)
+					if newer1_addr is not None:
+						print (newer1_addr.latlng)
+					else:	
+						print (addr+"---------"+ row[2]+","+"NEW ADDRESS FAILED")
+		else:
+			print ("CORRECT")
+			#print addr_str+","+"NEW YORK"
 		
-		print point+","+loc1.address
+	#	print point+","+loc1.address
+		
+		#print(type(loc1.address))
 
 		#print loc1.address
 		#if(loc)
