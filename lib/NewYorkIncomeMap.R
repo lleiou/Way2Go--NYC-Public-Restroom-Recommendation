48117c10bd8c43a6872378e1889e949077bfb50c
AndyWanColumbia <zw2327@columbia.edu>
2016-02-22 21:14:55 -0500

    why i am so diao

--- lib/NewYorkIncomeMap.R
+++ lib/NewYorkIncomeMap.R
@@ -22,33 +22,37 @@ income<-acs.fetch(endyear = 2012, span = 5, geography = geo,
 
 names(attributes(income))
 attr(income, "acs.colnames")
-income_df <- data.frame(paste0(str_pad(income@geography$state, 2, "left", pad="0"), 
+income_df1 <- data.frame(paste0(str_pad(income@geography$state, 2, "left", pad="0"), 
                                str_pad(income@geography$county, 3, "left", pad="0"), 
                                str_pad(income@geography$tract, 6, "left", pad="0")), 
                         income@estimate[,c("Household Income: Total:",
                                            "Household Income: $200,000 or more")], 
                         stringsAsFactors = FALSE)
 
-income_df <- select(income_df, 1:3)
-rownames(income_df)<-1:nrow(income_df)
-names(income_df)<-c("GEOID", "total", "over_200")
-income_df$percent <- 100*(income_df$over_200/income_df$total)
 
-income_merged<- geo_join(tracts, income_df, "GEOID", "GEOID")
+income_df1=cbind()
+
+
+income_df1 <- select(income_df1, 1:4)
+rownames(income_df1)<-1:nrow(income_df1)
+names(income_df1)<-c("Census Name","GEOID", "total", "over_200")
+income_df1$percent <- 100*(income_df1$over_200/income_df1$total)
+
+income_merged1<- geo_join(tracts, income_df1, "GEOID", "GEOID")
 # there are some tracts with no land that we should exclude
-income_merged <- income_merged[income_merged$ALAND>0,]
+income_merged1 <- income_merged1[income_merged1$ALAND>0,]
 
 
 
-popup <- paste0("GEOID: ", income_merged$GEOID, "<br>", "Percent of Households above $200k: ", round(income_merged$percent,2))
+popup <- paste0("Census: ", income_merged1$Census.Name, "<br>", "Percent of Households above $200k: ", round(income_merged$percent,2))
 pal <- colorNumeric(
   palette = "YlGnBu",
-  domain = income_merged$percent
+  domain = income_merged1$percent
 )
 
 map3<-leaflet() %>%
   addProviderTiles("CartoDB.Positron") %>%
-  addPolygons(data = income_merged, 
+  addPolygons(data = income_merged1, 
               fillColor = ~pal(percent), 
               color = "#b2aeae", # you need to use hex colors
               fillOpacity = 0.7, 
@@ -56,10 +60,12 @@ map3<-leaflet() %>%
               smoothFactor = 0.2,
               popup = popup) %>%
   addLegend(pal = pal, 
-            values = income_merged$percent, 
+            values = income_merged1$percent, 
             position = "bottomright", 
             title = "Percent of Households<br>above $200k",
             labFormat = labelFormat(suffix = "%")) 
+
 map3
+
 
 
