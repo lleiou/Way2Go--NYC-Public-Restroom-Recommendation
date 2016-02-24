4dbd5a17362866763b2c5970a9edfec90586722a
hy2450 <hy2450@columbia.edu>
2016-02-23 20:31:22 -0500

    picture

--- output/leaflet.R
+++ output/leaflet.R
@@ -1,13 +1,14 @@
 library(maps)
 library(data.table)
+library(dplyr)
 library(leaflet)
 library(htmltools)
 
-tdata<-read.csv("NYCT_NEW.csv")
+tdata<-read.csv("NYCT_NEW_ZIP.csv")
 wifidata<-read.csv("nycwifi.csv")
+#fdata<-read.csv("NYC_fac.csv")
 
-
-
+##restrooms popup
 name <-tdata$Name
 location <- tdata$Location
 handicap<-tdata$Handicap
@@ -17,7 +18,25 @@ location <- paste("<i>",location,"</i>")
 handicap <- paste("<i>","handicap:",handicap,"</i>")
 content <- paste( name,location,handicap, sep =  "<br/>")
 
-#set parameters for the icon
+##facilities popup
+facname <-fdata$FacName
+facaddress<- fdata$FacAddress
+ft_decode<-fdata$FT_Decode
+#separate the location name(bold) and the address(italic) into two lines
+facname <- paste("<b>",facname,"</b>")
+facaddress <- paste("<i>",facaddress,"</i>")
+ft_decode <- paste("<i>",ft_decode,"</i>")
+fcontent <- paste( facname,facaddress,ft_decode, sep =  "<br/>")
+
+
+
+
+
+
+
+
+
+#set parameters for the iconn
 #the iconAnchor is used for setting the position of the tag when you click on the toilet botton.
 ToiletIcon <- makeIcon(
   iconUrl = "http://icons.iconarchive.com/icons/icons8/ios7/512/Household-Toilet-Pan-icon.png",
@@ -26,14 +45,15 @@ ToiletIcon <- makeIcon(
 )
 
 map<-leaflet()%>%
-  setView(lng=-73.96884112664793,lat =40.78983730268673, zoom=11) %>% 
+  setView(lng=-73.96884112664793,lat =40.78983730268673, zoom=15) %>% 
   addTiles() %>% 
   addMarkers(data =tdata, group = "Restrooms",popup = content, icon=ToiletIcon) %>%
   addMarkers(data = wifidata, group = "Wi-Fi",lng = ~ Long_, lat = ~ Lat,
              popup = ~htmlEscape(Location), clusterOptions = markerClusterOptions())%>%
+  #addMarkers(data =data, group = "Facilities",popup = fcontent) %>%
   # Layers control
     addLayersControl(
-    overlayGroups = c("Restrooms", "Wi-Fi"),
+    overlayGroups = c("Restrooms", "Wi-Fi","Facilities"),
   
     options = layersControlOptions(collapsed = FALSE)
     )
--- output/ui.R
+++ output/ui.R
@@ -14,11 +14,11 @@ library(DT)
 header <- dashboardHeader(title = "Search for Restroom!")
 siderbar<-dashboardSidebar(
         sidebarMenu(
-                menuItem("Locate", tabName = "map", icon = icon("fa fa-map-marker"),badgeLabel = "TryMe!", badgeColor = "green"),
+                menuItem("Map", tabName = "map", icon = icon("fa fa-map"),badgeLabel = "TryMe!", badgeColor = "green"),
                 menuItem("Statistic Analysis",tabName = "stats", icon = icon("fa fa-bar-chart"), 
                          
-                         menuSubItem("Density Map",tabName = "chart1",icon = icon("fa fa-map")),
-                         menuSubItem("Income map",tabName = "chart2",icon = icon("fa fa-usd")))
+                         menuSubItem("Density Map",tabName = "chart1"),
+                         menuSubItem("Income map",tabName = "chart2"))
                    )
                           )
 body <- dashboardBody(
@@ -32,22 +32,25 @@ body <- dashboardBody(
                                                 )
                                             
                                             ),
-                                     column(width = 3,height= 5,
+                                     column(width = 3,
                                            box(width = NULL, status = "warning",
                                                 textInput("address", "My location:"),
                                                 actionButton("go", "Go!"),
-                                                 p("Click the button to update the value displayed in the main panel.") ),
-                                            box(
-                                        width = NULL, status = "warning",
+                                                 p("Click the button to update the value displayed in the main panel.") 
+                                            ),
+                                            box(width = NULL, status = "warning",
                                                  sliderInput("range", "Choose a range", 1, 2000,1000),
                                                 p("Distance in meter")
                         
-                                             ))
-                                     ),
-                            
+                                             ),
+                                           fluidRow(width=150, height=80,
+                                               actionButton("mylocation", "My Location"),
+                                               p("Click the button to update the value displayed in the main panel."), 
+                                               img(src = "images.png",width=80, height=80))
+                                        )
+                            ),
                           fluidRow(box(title = "table",width=800,DT::dataTableOutput("table")) )           
-                         
-                          ),
+                         ),
         #Second tab Item
               tabItem(tabName = "chart1",
                       fluidRow(
@@ -55,10 +58,10 @@ body <- dashboardBody(
                                       title = "",
                                       # The id lets us use input$tabset1 on the server to find the current tab
                                       id = "tabset1", 
-                                      tabPanel("Density map of Restrooms", plotOutput("plot1")),
+                                      tabPanel("Tab1", plotOutput("plot1")),
                                       tabPanel("Tab2", "Tab content 2"),
-                                      tabPanel("Tab3", "Tab content 3"),
-                              width = 720,height=600)
+                                      tabPanel("Tab3", "Tab content 3")
+                              )
                             
                       )),
                       tabItem(tabName="chart2",
@@ -67,7 +70,6 @@ body <- dashboardBody(
                               ))  
         )
 )
-
 
 
 ui<-dashboardPage(header,siderbar,body, title = "Simple Shiny",skin="purple")
